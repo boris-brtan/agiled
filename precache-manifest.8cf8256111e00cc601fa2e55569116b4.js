4a6da14c3fd4f2fdd1a3e1c96c7c915ac161e29d
Boris Brtáň <39240194+boris-brtan@users.noreply.github.com>
2020-01-02 16:30:41 +0100

    Updates

---
+++ precache-manifest.8cf8256111e00cc601fa2e55569116b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd3783056bb482dceb822bb11ace5bf",
+    "url": "/agiled/index.html"
+  },
+  {
+    "revision": "c89ea3f46eee277fb4ae",
+    "url": "/agiled/static/css/main.bf3b739f.chunk.css"
+  },
+  {
+    "revision": "842f8158b148a56b70de",
+    "url": "/agiled/static/js/2.81049317.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/agiled/static/js/2.81049317.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c89ea3f46eee277fb4ae",
+    "url": "/agiled/static/js/main.7b3e54b1.chunk.js"
+  },
+  {
+    "revision": "a473761597438fc7bcbd",
+    "url": "/agiled/static/js/runtime-main.4089214a.js"
+  },
+  {
+    "revision": "c97ce8b06d953d12149b383a1f07aa4f",
+    "url": "/agiled/static/media/alarm.c97ce8b0.mp3"
+  },
+  {
+    "revision": "9c808491a1cb3dc4720f2d917f8d819c",
+    "url": "/agiled/static/media/tick.9c808491.mp3"
+  }
+]);
